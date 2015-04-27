ea2c349a18698f91cb59f03c30b8765151c05a17
Alessandro Donato <alex.donato@gmail.com>
2015-04-28 00:02:12 +0200

    aggiunta security e form gestita con angularjs

--- esempi-spring-angular/src/main/java/it/alexdonato/toolbox/HomeController.java
+++ esempi-spring-angular/src/main/java/it/alexdonato/toolbox/HomeController.java
@@ -14,8 +14,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -60,4 +63,26 @@ public class HomeController {
 		return "home";
 	}	
 	
+	@RequestMapping (
+		value = "/admin/**",
+		method = RequestMethod.GET
+		)	
+	public ModelAndView goToAdmin () {
+		ModelAndView model = new ModelAndView();
+		model.addObject("nome", "cippa");
+		model.setViewName("admin/protected");
+		return model;
+	}
+	
+	@RequestMapping (
+			value = "/salva-domande",
+			method = RequestMethod.POST,
+			produces = "application/json"
+			)
+	public @ResponseBody DomandaBean salvaDomande (@RequestBody DomandaBean domanda) {
+		logger.info("entrato: Benvenuto " + domanda);
+		domanda.setDomanda("Sei un coglione?");
+		domanda.setRisposta("NOOOOOO");
+		return domanda;
+	}
 }
